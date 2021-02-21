a956b4134b802b995767e8bf754e8de96724e4c4
Bobby Prabowo <bobby.prabowo@quipper.com>
2021-02-21 19:26:52 +0700

    Switch to ios 14

---
+++ iosApp/iosApp/VideoPlaylistApp.swift
@@ -0,0 +1,19 @@
+//
+//  VideoPlaylistApp.swift
+//  iosApp
+//
+//  Created by Bobby Prabowo on 21/02/21.
+//  Copyright © 2021 orgName. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct VideoPlaylistApp: App {
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
