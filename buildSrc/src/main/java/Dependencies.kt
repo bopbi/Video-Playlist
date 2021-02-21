738da2235d57bcbabe9d40facbb6fd837cc570b2
Bobby Prabowo <bobby.prabowo@quipper.com>
2021-02-21 19:10:43 +0700

    fix kotlin serialization when build for iOS, downgrade to 1.0.1

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -3,7 +3,7 @@ object Versions {
     const val kotlin = "1.4.30"
     const val sqlDelight = "1.4.4"
     const val ktor = "1.5.0"
-    const val kotlinxSerialization = "1.1.0"
+    const val kotlinxSerialization = "1.0.1"
     const val kotlinCoroutines = "1.4.2-native-mt"
     const val lifecycleVersion = "2.3.0"
 }
